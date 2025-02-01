e9b19f89cd45fd7f952761e18bd1bed8cad0a723
Sofiia <bratanovska.sophiia@gmail.com>
2025-02-01 21:35:50 +0200

    cleaned up the code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function sayHello(name,surname, someOtherWord){
-    //some comment change
+
     console.log(`Hello! ${name} ${surname} ${someOtherWord}`);
 }
-//end function
+
 sayHello('Robot', 'Dreams');
-//done
