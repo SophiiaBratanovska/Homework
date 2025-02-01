0e0bbe603deb483203a4ce0f7acbbfa7f2d82039
Sofiia <bratanovska.sophiia@gmail.com>
2025-02-01 22:04:53 +0200

    Finish commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function sayHello(name,surname, someOtherWord){
-
+    //some comment change
     console.log(`Hello! ${name} ${surname} ${someOtherWord}`);
 }
-
+//end function
 sayHello('Robot', 'Dreams');
+//done
